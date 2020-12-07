d3ee177b8a4f15d22364fc1f5f8ec39e9e603d00
timchen0618 <b05901101@ntu.edu.tw>
2020-12-07 12:23:32 +0800

    display retrieved items

--- app/control.py
+++ app/control.py
@@ -1,5 +1,7 @@
 import re
 import json
+import pandas as pd
+import numpy as np
 
 
 def read_json(filename):
@@ -10,14 +12,42 @@ def read_json(filename):
 
 class Controller(object):
     """docstring for Controller"""
-    def __init__(self, intent_pattern, entity_info, resp_info):
+    def __init__(self, intent_pattern, entity_info, styleme_processed, effect2ids, resp_info):
         super(Controller, self).__init__()
         self.intent_pattern = read_json(intent_pattern)
         self.entity_info = read_json(entity_info)
+        self.item_info = pd.read_csv(styleme_processed, sep='\t')
+        self.effect2ids = read_json(effect2ids)
         self.resp_info = read_json(resp_info)
+        
         self.regex = {}
         self.prepare_regex()
-        # print(self.regex['search_item'][16])
+        self.num_rand_product = 5
+        self.neccess_info = [1, 2, 4, 6]
+        self.prepare_item_regex()
+
+    def prepare_item_regex(self):
+        self.items_regex = {}
+        for l in self.entity_info['item'][0]:
+            str_list = []
+            for item_name in self.entity_info[l][1]:
+                str_list.append('(' + item_name + ')')
+            self.items_regex[l] = '|'.join(str_list)
+
+        self.brands_regex = {}
+        for l in self.entity_info['brand'][0]:
+            str_list = []
+            for brand_name in self.entity_info[l][1]:
+                str_list.append('(' + brand_name + ')')
+            self.brands_regex[l] = '|'.join(str_list)
+
+        self.effects_regex = {}
+        for l in self.entity_info['effect'][0]:
+            str_list = []
+            for effect_name in self.entity_info[l][1]:
+                str_list.append('(' + effect_name + ')')
+            self.effects_regex[l] = '|'.join(str_list)
+
     def prepare_regex(self):
         # loop thru every items
         find_paren_exp = r'\(([\w\|]*)\)'
@@ -30,9 +60,6 @@ class Controller(object):
                 pattern = p
                 m = re.findall(find_paren_exp, p)
                 if m:
-                    # print('---------------------')
-                    # print('pattern', p)
-                    # print(m)
                     for subreg in m:
                         # if subreg[:5] == 'brand' or subreg[:4] == 'name':
                         #     continue
@@ -52,12 +79,8 @@ class Controller(object):
                             # if ent == 'effect':
                             #     print('effect')
                             #     print('subre', subreg_expand)
-                        # if k == 'search_item' and p == "(有沒有)(推薦)(的)?(brand|color|effect)?(的)?(item|商品_commodity)":
-                        #     print('subref', subreg_expand)
                         pattern = pattern.replace(subreg, subreg_expand)
 
-                # print('*'*50)
-                # print(pattern)
                 try:
                     compiled = re.compile(pattern)
                 except:
@@ -89,8 +112,6 @@ class Controller(object):
     def check_item_brand(self, item):
         try:
             senses, terms = self.entity_info[item]
-            # print('senses', senses)
-            # print('terms', terms)
         except:
             return item
         sense_str_list = []
@@ -111,6 +132,8 @@ class Controller(object):
         match_str = ""
         match_intent = None
         match_idx = 0
+        get_item = False
+        items = []
         for intent, reg_list in self.regex.items():
             for i, reg in enumerate(reg_list):
                 search_str = re.search(reg, cmd)
@@ -120,16 +143,103 @@ class Controller(object):
                         match_intent = intent
                         match_idx = i
         if match_str == "":
-            return "nomatch", "NO PATTERNS FOUND...", ""
+            return "nomatch", "NO PATTERNS FOUND...", False, [], ""
         else:
             print('match_str', match_str)
-            return match_intent, self.intent_pattern[match_intent][match_idx], self.resp_info[match_intent][match_idx][0]
+            if match_intent == 'search_item':
+                get_item = True
+                items = self.get_items(cmd, match_idx not in [5, 6, 17], match_idx in [7, 8, 9, 10, 11, 12, 13, 14, 15, 18, 19])
+                print('items', items)
+                items_proc = self.process_item(items)
+                print('processed', items_proc)
+                items = items_proc
+
+            return match_intent, self.intent_pattern[match_intent][match_idx], get_item, items, self.resp_info[match_intent][match_idx][0]
 
     def control(self, cmd):
-        cmd_string, pattern_string, resp_string = self.check_intent(cmd)
-        return cmd_string, pattern_string, resp_string
+        cmd_string, pattern_string, get_item, items, resp_string = self.check_intent(cmd)
+        return cmd_string, pattern_string, get_item, items, resp_string
 
-ctrl = Controller('app/pattern/intent_pattern.json', 'app/pattern/entity_info.json', 'app/pattern/response.json')
+    def get_items(self, cmd, specify_item, brand_or_effect):
+        items = self.item_info
+        if specify_item:
+            item_list = []
+            if brand_or_effect:
+                for effect, regex in self.effects_regex.items():
+                    search_str = re.search(regex, cmd)
+                    # means belong to a brand
+                    if search_str:
+                        ids = self.effect2ids[effect]
+                        items = items.iloc[ids]
+                        print('effect', effect)
+                        break
+
+                for brand, regex in self.brands_regex.items():
+                    search_str = re.search(regex, cmd)
+                    # means belong to a brand
+                    if search_str:
+                        items = items[items['brand'] == brand]
+                        print('brand', brand)
+                        break
+
+                
+
+            # check for if item
+            for item_type, regex in self.items_regex.items():
+                search_str = re.search(regex, cmd)
+                # means belong to an item
+                if search_str:
+                    items = items[items['item'] == item_type]
+                    print('item', item_type)
+                    break
+
+            
+
+            return self.random_return(df=items, size=20)
+            # else:
+            #     return self.random_return(df=items, size=20)
+        else:
+            return self.random_return()
+
+    def random_return(self, df=None, size=None):
+        # all random
+        if not isinstance(df, pd.DataFrame):
+            rand_nums = np.random.randint(self.item_info.shape[0], size=self.num_rand_product)
+            items = self.item_info.iloc[rand_nums, self.neccess_info]
+            print(items)
+            item_list = list(items.to_records(index=False))
+            return item_list
+
+        if not size:
+            size = self.num_rand_product
+
+        # if specify df
+        if df.shape[0] <= size:
+            return list(df.iloc[:, self.neccess_info].to_records(index=False))
+        else:
+            rand_nums = np.random.randint(df.shape[0], size=size)
+            items = df.iloc[rand_nums, self.neccess_info]
+            item_list = list(items.to_records(index=False))
+            return item_list
+            
+    def process_item(self, items):
+        if not items:
+            return []
+        else:
+            new_items = []
+            for item in items:
+                if isinstance(item[1], str) and isinstance(item[3], str):
+                    if isinstance(item[0], str):
+                        new_items.append((item[0].replace('_', ' '), item[1], item[3]))
+                    else:
+                        new_items.append(('', item[1], item[3]))
+
+            return new_items        
+        
+
+
+ctrl = Controller('app/pattern/intent_pattern.json', 'app/pattern/entity_info.json', 'app/pattern/styleme_new.tsv', 'app/pattern/effect2ids.json', 'app/pattern/response.json')
+
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -28,8 +28,10 @@ def index():
     pattern_string = ""
     resp_string = ""
 
+    get_item = False
     pattern = request.args.get('pattern')
     suggestions = []
+    item_list = []
     if pattern:
         sug = ctrl.intent_pattern[pattern]
         for s in sug:
@@ -42,17 +44,34 @@ def index():
         print('fff')
         command = form.inputtext.data
         flash('已輸入指令: {}'.format(command))
-        cmd_string, pattern_string, resp_string = ctrl.control(command)
+
+        cmd_string, pattern_string, get_item, item_list, resp_string = ctrl.control(command)
+
         zh_string = ""
         for k in CMD_LIST:
             if k[0] == cmd_string:
                 zh_string = k[1]
-        flash('Pattern: [{} {}] {}'.format(cmd_string, zh_string, pattern_string))
 
+        flash('Intention: {} {}'.format(cmd_string, zh_string))
+        flash('Pattern:   {}'.format(pattern_string))
+        # flash('Pattern: [{} {}] {}'.format(cmd_string, zh_string, pattern_string))
         # return redirect(url_for(cmd_string))
         print(cmd_string)
-        return render_template('index.html', title='Intention Demo', form=form, cmd=cmd_string, cmd_list=CMD_LIST, suggestions=suggestions, resp_string=resp_string)
-    return render_template('index.html', title='Intention Demo', form=form, cmd="", cmd_list=CMD_LIST, suggestions=suggestions, resp_string=resp_string)
+        return render_template('index.html', title='Intention Demo', form=form, cmd=cmd_string, cmd_list=CMD_LIST, suggestions=suggestions, get_item=get_item, item_list=item_list, resp_string=resp_string)
+    return render_template('index.html', title='Intention Demo', form=form, cmd="", cmd_list=CMD_LIST, suggestions=suggestions, get_item=get_item, item_list=item_list, resp_string=resp_string)
+
+@app.route('/items', methods=['GET', 'POST'])
+def items():
+    num_form = (len(request.form))//3
+    print(num_form)
+    item_list = []
+    form_table = request.form.to_dict(flat=False)
+    print('table', form_table)
+    for i in range(num_form):
+        # print(form_table['attr-%d'%i], form_table['value-%d'%i])
+        item_list.append((form_table['brand-%d'%i], form_table['name-%d'%i], form_table['image-%d'%i]))
+
+    return render_template('items.html', item_list = item_list)
 
 
 # @app.route('/aboutYou', methods=['GET', 'POST'])
